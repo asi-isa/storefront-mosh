a629611fed3006632d52fd9c5d7e2921371bcbc3
asi-isa <ali.inceoglu@protonmail.com>
2026-09-24 14:56:41 +0200

    add Tag and TaggedItem models for tagging functionality

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+
+# What tag is applied to what item?
+class TaggedItem(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    # we need to know the type(table) and the id of an object to identify it in our application
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    # we assume that the id is a positive integer
+    object_id = models.PositiveIntegerField()
+    # actual object that this tag is applied to
+    content_object = GenericForeignKey()
